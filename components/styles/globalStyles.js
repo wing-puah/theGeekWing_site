4b6578416e44c6758f98b588521cc10487d89542
wing-puah <puahhuiying@gmail.com>
2021-04-26 18:02:26 +0800

    install dependencies

---
+++ components/styles/globalStyles.js
@@ -0,0 +1,164 @@
+import { createGlobalStyle, css } from 'styled-components';
+import theme, { device } from 'theme';
+
+const tableCellMixin = css`
+  border-right: 1px solid ${theme.palette.grey[400]};
+  padding: 2px 8px;
+
+  &:last-child {
+    border-right: 0;
+  }
+`;
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Exo:400,900|Open+Sans:400,400i,700,700i&display=swap');
+
+  body {
+    color: ${theme.palette.text.primary};
+    position: relative;
+    overflow-x: hidden;
+    font-size: ${theme.typography.body1.fontSize};
+  }
+
+  html, body {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Exo', sans-serif;
+  }
+
+  h2, h3, h4, h5, h6 {
+    padding-top: 25px;
+    
+    ${device.laptop} {
+      padding-top: 40px; 
+    }
+  }
+
+  h1, h2, h3 {
+    font-weight: 900;
+  }
+
+  h4,h5,h6 {
+    font-weight: 400;
+  } 
+
+  h1 {
+    font-size: 1.8rem;
+
+    ${device.laptop} {
+      font-size: 3rem; 
+    }
+  }
+
+  h2 {
+    font-size:${theme.typography.h2.fontSize}; 
+
+    ${device.laptop} {
+      font-size: ${theme.typography.h2[device.laptop].fontSize}; 
+    }
+  }
+
+  a {
+    color: ${theme.palette.primary.dark};
+  }
+
+  p {
+    line-height: 1.8;
+  }
+
+  .width--full {
+    width: 100%;
+  }
+
+  .height--full {
+    height: 100%;
+  }
+
+  blockquote {
+    padding: 10px;
+    margin-left: 10px;
+    position: relative; 
+    color: ${theme.typography.blockquote.fontColor};
+    line-height: ${theme.typography.blockquote.lineHeight};
+
+    ${theme.breakpoints.up.laptop} {
+      margin-left: 30px; 
+      padding: 20px;
+    }
+
+    p:last-child {
+      margin-bottom: 0;
+    }
+
+    &:before {
+      content: ''; 
+      position: absolute; 
+      width: 10px; 
+      height: 100%; 
+      background: ${theme.palette.highlight.main};
+      left: -10px;
+      top: 0;
+    }
+
+    &:after {
+      content: ''; 
+      position: absolute; 
+      top: 0;
+      left: 0;
+      width: 100%; 
+      height: 100%; 
+      border: 1px solid ${theme.palette.highlight.main};
+      z-index: -1;
+    }
+  }
+
+  img {
+    width: 100%;
+  }
+
+  iframe {
+    max-width: 100%;
+  }
+
+  pre {
+    code {
+      display: block;
+      padding: 0.5rem 1rem;
+      overflow: auto;
+      border-radius: 0.3rem;
+      background: hsl(24, 20%, 95%);
+
+      ${theme.breakpoints.up.tablet} {
+        padding: 2rem; 
+      }
+    }
+  }
+
+  .code {
+    label {
+      font-family: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+      margin-bottom: 0;
+    }
+  }
+
+  table {
+    border: 1px solid ${theme.palette.grey[400]};
+
+    thead {
+      border-bottom: 2px solid ${theme.palette.grey[300]};
+
+      th {
+        ${tableCellMixin}
+      }
+    } 
+
+    tbody {
+      td { ${tableCellMixin} }
+    }
+  }
+`;
+
+export default GlobalStyles;
---
+++ components/styles/theme.js
@@ -0,0 +1,170 @@
+const breakpointsValue = {
+  mobileS: 320,
+  mobileM: 375,
+  mobileL: 425,
+  tablet: 768,
+  laptop: 1024,
+  laptopL: 1440,
+  desktop: 2560,
+};
+
+const mediaWidthRules = (rule, breakpoint) => {
+  const widthMinOrMaxRules = {
+    up: 'min',
+    down: 'max',
+  };
+  const _rule = rule ? widthMinOrMaxRules[rule] : widthMinOrMaxRules.up;
+  return `${_rule}-width: ${breakpoint}px`;
+};
+
+const breakpointsMixin = rule => {
+  return Object.keys(breakpointsValue).reduce((acc, cur) => {
+    acc[cur] = `@media only screen and (${mediaWidthRules(
+      rule,
+      breakpointsValue[cur]
+    )})`;
+    return acc;
+  }, {});
+};
+
+const breakpoints = {
+  values: breakpointsValue,
+  up: breakpointsMixin('up'),
+  down: breakpointsMixin('down'),
+};
+
+const device = Object.keys(breakpointsValue).reduce((acc, cur) => {
+  acc[cur] = `@media only screen and (min-width: ${breakpointsValue[cur]}px)`;
+  return acc;
+}, {});
+
+const palette = {
+  primary: {
+    light: '#35BDF2',
+    main: '#0442BF',
+    dark: '#056CF2',
+    contrastText: '#F2F2F2',
+  },
+  secondary: {
+    light: '#F2C166',
+    main: '#F29F05',
+    dark: '#F26835',
+    contrastText: '#F2F2F2',
+  },
+  highlight: {
+    light: '#0CF22F',
+    main: '#04BF68',
+    dark: '#02402E',
+  },
+  error: {
+    light: '#E10000',
+    main: '#BA0000',
+    dark: '#950000',
+  },
+  grey: {
+    50: '#EEEEEE',
+    100: '#dddddd',
+    200: '#888888',
+    300: '#4C5359',
+    400: '#232526',
+  },
+  common: {
+    white: '#fff',
+    black: '#222',
+  },
+  text: {
+    primary: '#222',
+    disabled: '#ddd',
+    hint: '#056CF2',
+  },
+};
+
+const fontFamily = {
+  header: `'Exo', sans-serif`,
+  body: `'Open Sans', sans-serif`,
+};
+
+const typography = {
+  fontWeightLight: 300,
+  fontWeightRegular: 400,
+  fontWeightMedium: 500,
+  fontWeightBold: 900,
+  fontFamily: fontFamily.body,
+  h1: {
+    fontFamily: fontFamily.header,
+    fontWeight: 900,
+    fontSize: '1.8rem',
+    [device.laptop]: {
+      fontSize: '3rem',
+    },
+  },
+  h2: {
+    fontSize: '1.5rem',
+    [device.laptop]: {
+      fontSize: '2rem',
+    },
+  },
+  body1: {
+    fontFamily: fontFamily.body,
+    fontWeight: 400,
+    fontSize: '1rem',
+  },
+  blockquote: {
+    fontColor: palette.grey[400],
+    lineHeight: 2,
+  },
+};
+
+const zIndex = {
+  appBar: 1100,
+  drawer: 1200,
+  modal: 1300,
+  snackbar: 1400,
+  tooltip: 1500,
+};
+
+const theme = {
+  palette,
+  border: {
+    light: palette.grey[50],
+    main: palette.grey[100],
+    dark: palette.grey[200],
+  },
+  button: {
+    primary: {
+      default: {
+        color: palette.secondary.main,
+        contrastText: palette.secondary.contrastText,
+      },
+      hover: {
+        color: palette.secondary.dark,
+        contrastText: palette.secondary.contrastText,
+      },
+    },
+    secondary: {
+      default: {
+        color: palette.primary.main,
+        contrastText: palette.primary.contrastText,
+      },
+      hover: {
+        color: palette.primary.dark,
+        contrastText: palette.primary.contrastText,
+      },
+    },
+  },
+  navigation: {
+    default: '#dddddd',
+    hover: '#155902',
+  },
+  footer: {
+    background: '#69168C',
+    color: '#F2F2F2',
+  },
+  breakpoints,
+  device,
+  zIndex,
+  typography,
+};
+
+export { device };
+export default theme;
