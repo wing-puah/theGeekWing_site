b37185fe5ed3a0565c6dc85316ad841b481956d9
wing-puah <puahhuiying@gmail.com>
2021-05-26 11:02:20 +0800

    add hero

---
+++ components/home/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1>Hello I am Wing</h1>
+    </div>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { getLayout } from 'layouts/BaseLayout';
 import { config } from 'components/home/config';
+import Hero from 'components/home/Hero';
 
 function Home() {
   return (
     <>
       {config.intro}
       <div className="p-3">
+        <Hero />
         <h1>New site coming soon</h1>
       </div>
     </>
